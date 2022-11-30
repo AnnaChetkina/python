a4b6138934ba0a1069e625a4a46f8600b05156b8
Anna <a.chetkina.a@yandex.ru>
2022-11-30 23:26:11 +0300

    add homework3 ver.3 and fix gitignore

--- homework3.py
+++ homework3.py
@@ -4,6 +4,8 @@ import math
 # стоящих на нечётной позиции.
 # *Пример:*
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+print('Задача 1:')
+
 l = [2, 3, 5, 9, 3, 8]
 
 
@@ -12,10 +14,10 @@ def odd_el_sum(l):
     for ind in range(0, len(l)):
         if ind % 2 == 1:
             summa += l[ind]
-    print(summa)
+    print(f'Сумма элементов списка {l}, стоящих на нечётной позиции = {summa}')
 
-print('Задача 1:')
 odd_el_sum(l)
+print()
 
 # 2'. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент,
 # второй и предпоследний и т.д.
@@ -23,28 +25,30 @@ odd_el_sum(l)
 # *Пример:*
 # - [2, 3, 4, 5, 6] =>[12,15,16]      ([2*6, 3*5, 4*4]);
 # - [2, 3, 5, 6] => [12,15]   ( [2*6, 3*5])
+print('Задача 2:')
 
 l1 = [2, 3, 4, 5, 6]
-l2 = [2, 3, 5, 6, 2]
+l2 = [2, 3, 5, 6]
 
 
 def mult_pairs(l):
     res = []
     for ind in range(0, math.ceil(len(l) / 2)):
         res.append(l[ind] * l[len(l) - 1 - ind])
-    print('res', res)
-
+    print(f'Произведение пар чисел списка {l} = {res}')
 
 mult_pairs(l1)
-print()
 mult_pairs(l2)
+print()
 
 
 # 3'. Задайте список из вещественных чисел. Напишите программу,
 # которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 # *Пример:*
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
-def get_truncate(elem):
+
+print('Задача 3:')
+def get_truncate_list(elem):
     el = str(elem)
     dot_ind = el.find('.')
     if dot_ind == -1:
@@ -54,20 +58,15 @@ def get_truncate(elem):
     return float(str_res)
 
 
-l3 = [1.1, 1.2, 3.1, 5.9, 10.01]
-
-
 def fn_diff(l):
-    res = list(map(get_truncate, l))
-    # min_el = min(list.copy(res))
-    # max_el = max(list.copy(res))
-    diff = max(list.copy(res)) - min(list.copy(res))
-    # print(min_el, max_el, diff)
-    print(diff)
+    res = list(map(get_truncate_list, l))
+    # diff = max(list.copy(res)) - min(list.copy(res))
+    diff = max([*res]) - min([*res])
+    print(f'Разница между максимальным и минимальным значением дробной части элементов списка {l} = {diff}')
 
-
-print()
+l3 = [1.1, 1.2, 3.1, 5.9, 10.01]
 fn_diff(l3)
+print()
 
 
 # 4'. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
@@ -75,6 +74,7 @@ fn_diff(l3)
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+print('Задача 4:')
 
 def get_binary(num, res: str):
     # print(num, res)
@@ -84,58 +84,47 @@ def get_binary(num, res: str):
         res = str(num % 2) + res
     return get_binary(int(num / 2), res)
 
-
+num = 15
+print(f"{num} = {get_binary(num, '')}")
+print(f'{num} = {bin(15)} # Проверка через встроенную функцию ')
 print()
 
-
-# print(get_binary(15, ''))
-# print(bin(15))
 
 # 5'. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.(Дополнительно)
 # *Пример:*
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 
+print('Задача 5:')
 def fib(num):
     res = []
     i = 0
     while i < num:
-        # print(i, 'i')
         if i == 0 or i == 1:
             res.append(i)
         if i > 1:
             prev = (res[i - 2] + res[i - 1])
             res.append(prev)
         i += 1
-    print(res, 'fib')
+    # print(res, 'fib')
     return res
 
 
-# fib(11)
-
-def negative_fib(fibb):
-    # print('вход в функцию negative_fib', fibb)
-    # neg_fibb = list.copy(fibb)
-    neg_fibb = fibb[:]
-    for i in range(1, len(fibb)):
-        # print(((-1)**i), 'множитель')
-        # print(fibb[i], 'fibb[i]')
-        neg_fibb[i] = ((-1) ** (i + 1)) * fibb[i]
-    # print(neg_fibb[::-1], 'neg')
-    neg_fibb.remove(0)
-    return neg_fibb[::-1]
+def negafib(fib):
+    neg_fib = [*fib]
+    for i in range(1, len(fib)):
+        neg_fib[i] = ((-1) ** (i + 1)) * fib[i]
+    # print(neg_fib[::-1], 'neg')
+    neg_fib.remove(0)
+    return neg_fib[::-1]
 
 
-# negative_fib(fib(11))
-
-# def join_fib_lists(fib(11), negative_fib(fib)):
-def join_fib_lists(fib, negative_fib):
-    # print(fib, negative_fib, 'join')
-    fib_list = [*negative_fib, *fib]
+def join_fib_lists(fib, negafib):
+    fib_list = [*negafib, *fib]
     print(fib_list, 'fib_list')
 
 
-fibb1 = fib(11)
-fibb2 = negative_fib(fibb1)
+fib1 = fib(11)
+negafib1 = negafib(fib1)
 print('[−55, 34, −21, 13, −8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]', 'WIKI')
 
-join_fib_lists(fibb1, fibb2)
+join_fib_lists(fib1, negafib1)
