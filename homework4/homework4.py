b19ad1f18977989a1725001a97938ecb0c481d5b
Anna <a.chetkina.a@yandex.ru>
2022-12-12 01:20:46 +0300

    homework4 ver.1

---
+++ homework4/homework4.py
@@ -0,0 +1,70 @@
+# 1) 10-9 округление до этого числа
+# 2) число делим на что без остатка, из делителей можно получить уникальные, если перевести в сэт (множество),
+# метод sympy
+# 3) используем сэт, и дополнительно лист чтобы упорядочить массив/ лист компрехеншенс
+#
+# 4) рэндж с 3 параметрами?
+# 5) обратить внимание на склеивание полиномов, чтобы они сложились по числам правильно! ф-ция simplify сложит
+#
+# 1'. Вычислить число Пи c заданной точностью d
+# *Пример:*
+# - при d = 0.001, π = 3.141
+# - при d = 0.0001, π = 3.1415
+import math
+
+import sympy
+
+
+def get_pi():
+    accuracy = input('Введите требуемую точность: ')
+    accuracy_len = len(accuracy) - 2
+    res_pi = format(math.pi, f'.{accuracy_len}f')
+    print(res_pi)
+# get_pi()
+
+
+# 2'. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# * 6 -> [1,2,3]
+# * 144 -> [2,3]
+
+def prime_factors(n):
+    prime_factors = []
+    i = 2
+    while i**2 <= n:
+        if n % i == 0:
+            prime_factors.append(i)
+            n = n / i
+        else:
+            i += 1
+    if (n > 1):
+        prime_factors.append(int(n))
+    print(set(prime_factors))
+# prime_factors(6)
+# prime_factors(63)
+
+# 3'. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+import random
+def non_repeating_values(values_count, values_range):
+    init_list = [random.randint(0, values_range) for elem in range(0, values_count)]
+    res_list = list(set(init_list))
+    return res_list
+# print(non_repeating_values(10, 10))
+
+# 4'. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:
+# k=2 => 2*x*2 + 4*x + 5 = 0 или x2 + 5 = 0 или 10*x*2 = 0
+def func(k):
+    coefficients = [random.randint(0, 100) for elem in range(0, k + 1)]
+    print('coefficients', coefficients)
+    eq = ''
+    for i in range(0, len(coefficients) - 1):
+        eq += f'{coefficients[i]}*x**{k - i}+'
+    eq += f'{coefficients[-1]}'
+    print(eq)
+    x = sympy.Symbol('x')
+    solutions = sympy.solve(eq, x)
+    print(solutions, ' = solution')
+print('Task 4')
+func(2)
+
